d6ce860d7e6950d21e4e6e61ebe13271dd97ba3b
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-01-18 01:06:32 +0800

    Tugas 2 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas2.js
@@ -0,0 +1,27 @@
+// var number
+var number = 10;
+
+// var string
+var string = "hello coding";
+
+// var Boolean
+var cm = 75;
+if (cm > 80) panjang = true;
+else panjang = false;
+
+// var null
+var kosong = "ada";
+kosong = null;
+
+// var undefined
+var undefined;
+
+// var array
+var angka = [1, 2, 3, 4, 5];
+
+console.log(number);
+console.log(string);
+console.log(panjang);
+console.log(kosong);
+console.log(undefined);
+console.log(angka[2]);
